330a709fdfe559f2fb174c549c142ed1c88033bf
Aleksandr Steblevets <fortibrinelike@gmail.com>
2022-12-24 16:13:07 +0000

    Added 5 lesson

---
+++ Lesson5/app/src/main/java/me/sashasteblevets/lesson5/SecondActivity.java
@@ -0,0 +1,29 @@
+package me.sashasteblevets.lesson5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
